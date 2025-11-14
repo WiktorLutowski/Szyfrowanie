0d57085ce1a831f75c1d6f1c0588793f106e050c
wlutowski <lutowski.wikto@szkola.elektrykplcok.ud.pl>
2025-11-14 12:08:16 +0100

    done the exam

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,17 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Win32;
+using System;
+using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Szyfrowanie
 {
@@ -23,6 +13,78 @@ namespace Szyfrowanie
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// Encrypts data using Caesar cipher. Author: Wiktor Lutowski
+        /// </summary>
+        /// <param name="key">Key used in cipher</param>
+        /// <param name="text">Text to encrypt</param>
+        /// <returns>Encrypted text</returns>
+        private string Encrpyt(int key, string text)
+        {
+            string letters = "abcdefghijklmnopqrstuvwxyz";
+
+            string result = "";
+
+            foreach (var textChar in text)
+            {
+                bool isUpper = char.IsUpper(textChar);
+
+                int charIndex = letters.IndexOf(char.ToLower(textChar));
+
+                // Skip other chars than letters
+                if (charIndex == -1)
+                {
+                    result += textChar;
+                    continue;
+                }
+
+                for (int i = 0; i < Math.Abs(key); i++)
+                {
+                    if (key > 0)
+                        charIndex++;
+                    else
+                        charIndex--;
+                }
+
+
+                // Loop index when it is too big or small
+                while (charIndex < 0)
+                    charIndex += letters.Length;
+
+                while (charIndex > letters.Length - 1)
+                    charIndex -= letters.Length;
+
+                // Check if an orginal char was in upper or lower case
+                if (char.ToLower(textChar) != textChar)
+                    result += char.ToUpper(letters[charIndex]);
+                else
+                    result += letters[charIndex];
+            }
+
+            return result;
+        }
+
+        private void Button_Click_Encrypt(object sender, RoutedEventArgs e)
+        {
+            // Ensure that the key is a number
+            if (!int.TryParse(keyTxtBox.Text, out int key))
+                key = 0;
+
+            resultTxtBlock.Text = Encrpyt(key, textTxtBox.Text);
+        }
+
+        private void Button_Click_Save(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(resultTxtBlock.Text))
+                return;
+
+            SaveFileDialog dialog = new SaveFileDialog();
+
+            // Show save file dialog and if it passed, save the file
+            if ((bool)dialog.ShowDialog())
+                File.WriteAllText(dialog.FileName, resultTxtBlock.Text);
         }
     }
 }
